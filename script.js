0c0feca1cd012b7bd23eea58f7adb221739828ac
Radhames Capellán <85122601+radhamesc-capellan@users.noreply.github.com>
2024-06-25 10:31:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ window.addEventListener("load", () => {
   let totalSeconds;
 
   function init() {
-    totalSeconds = Math.floor((new Date("07/30/2022") - new Date()) / 1000);
+    totalSeconds = Math.floor((new Date("06/30/2024") - new Date()) / 1000);
     setTimeLeft();
     let interval = setInterval(() => {
       if (totalSeconds < 0) {
